47d6014c0ef9e2ed76b207add69b0636673f5363
Kaya Woods <kayanevada@gmail.com>
2025-01-30 12:41:05 -0800

    5

--- app.js
+++ app.js
@@ -217,7 +217,7 @@ Exercise 5: Array.prototype.map()
 
 Use the Array.prototype.map() method to reformat each name in the 'people' 
 array. The goal is to convert names from "Last, First" format to "First Last" 
-format.
+format
 
 Hint: Use the String.prototype.split() method to separate the first and last 
       names. You can split the string using ', ' as the separator.
@@ -227,48 +227,52 @@ Hint: Use the String.prototype.split() method to separate the first and last
 // let firstLast = [];
 
 // Complete the exercise in the space below:
-const firstLast = people.map()
+const firstLast = people.map((person) => {
+    const [last, first] = person.split (',') 
+    return `${last} ${first}`
+
+});
 // Check your work:
 console.log('Exercise 5 my result: ', firstLast);
-console.log('Exercise 5 correct result: ', [
-  'Carl Becker',
-  'Samuel Beckett',
-  'Mick Beddoes',
-  'Henry Beecher',
-  'Ludwig Beethoven',
-  'Menachem Begin',
-  'Hilaire Belloc',
-  'Saul Bellow',
-  'Robert Benchley',
-  'Peter Benenson',
-  'David Ben-Gurion',
-  'Walter Benjamin',
-  'Tony Benn',
-  'Chester Bennington',
-  'Leana Benson',
-  'Silas Bent',
-  'Lloyd Bentsen',
-  'Ric Berger',
-  'Ingmar Bergman',
-  'Luciano Berio',
-  'Milton Berle',
-  'Irving Berlin',
-  'Eric Berne',
-  'Sandra Bernhard',
-  'Yogi Berra',
-  'Halle Berry',
-  'Wendell Berry',
-  'Erin Bethea',
-  'Aneurin Bevan',
-  'Ken Bevel',
-  'Joseph Biden',
-  'Ambrose Bierce',
-  'Steve Biko',
-  'Josh Billings',
-  'Frank Biondo',
-  'Augustine Birrell',
-  'Elk Black',
-  'Robert Blair',
-  'Tony Blair',
-  'William Blake',
-]);
+// console.log('Exercise 5 correct result: ', [
+//   'Carl Becker',
+//   'Samuel Beckett',
+//   'Mick Beddoes',
+//   'Henry Beecher',
+//   'Ludwig Beethoven',
+//   'Menachem Begin',
+//   'Hilaire Belloc',
+//   'Saul Bellow',
+//   'Robert Benchley',
+//   'Peter Benenson',
+//   'David Ben-Gurion',
+//   'Walter Benjamin',
+//   'Tony Benn',
+//   'Chester Bennington',
+//   'Leana Benson',
+//   'Silas Bent',
+//   'Lloyd Bentsen',
+//   'Ric Berger',
+//   'Ingmar Bergman',
+//   'Luciano Berio',
+//   'Milton Berle',
+//   'Irving Berlin',
+//   'Eric Berne',
+//   'Sandra Bernhard',
+//   'Yogi Berra',
+//   'Halle Berry',
+//   'Wendell Berry',
+//   'Erin Bethea',
+//   'Aneurin Bevan',
+//   'Ken Bevel',
+//   'Joseph Biden',
+//   'Ambrose Bierce',
+//   'Steve Biko',
+//   'Josh Billings',
+//   'Frank Biondo',
+//   'Augustine Birrell',
+//   'Elk Black',
+//   'Robert Blair',
+//   'Tony Blair',
+//   'William Blake',
+// ]);
