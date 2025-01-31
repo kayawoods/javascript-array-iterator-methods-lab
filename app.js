c487a121e54ec87e004c5689f2e48e216f900a63
Kaya Woods <kayanevada@gmail.com>
2025-01-31 10:17:52 -0800

    corrected 2 and 5

--- app.js
+++ app.js
@@ -131,25 +131,28 @@ Hint: Return a new object literal from the callback that looks like:
 // Complete the exercise in the space below:
 
 const inventorNames = inventors.map((inventor) => {
-    return `${inventor.first}, ${inventor.last}`
+  const inventorObj  = {}
+  inventorObj.first = inventor.first 
+  inventorObj.last = inventor.last 
+    return inventorObj
 }) 
 
 // Check your work:
 console.log('Exercise 2 my result: ', inventorNames);
-// console.log('Exercise 2 correct result: ', [
-//   { first: 'Albert', last: 'Einstein' },
-//   { first: 'Isaac', last: 'Newton' },
-//   { first: 'Galileo', last: 'Galilei' },
-//   { first: 'Marie', last: 'Curie' },
-//   { first: 'Johannes', last: 'Kepler' },
-//   { first: 'Nicolaus', last: 'Copernicus' },
-//   { first: 'Max', last: 'Planck' },
-//   { first: 'Katherine', last: 'Blodgett' },
-//   { first: 'Ada', last: 'Lovelace' },
-//   { first: 'Sarah E.', last: 'Goode' },
-//   { first: 'Lise', last: 'Meitner' },
-//   { first: 'Hanna', last: 'Hammarström' },
-// ]); 
+console.log('Exercise 2 correct result: ', [
+  { first: 'Albert', last: 'Einstein' },
+  { first: 'Isaac', last: 'Newton' },
+  { first: 'Galileo', last: 'Galilei' },
+  { first: 'Marie', last: 'Curie' },
+  { first: 'Johannes', last: 'Kepler' },
+  { first: 'Nicolaus', last: 'Copernicus' },
+  { first: 'Max', last: 'Planck' },
+  { first: 'Katherine', last: 'Blodgett' },
+  { first: 'Ada', last: 'Lovelace' },
+  { first: 'Sarah E.', last: 'Goode' },
+  { first: 'Lise', last: 'Meitner' },
+  { first: 'Hanna', last: 'Hammarström' },
+]); 
 
 /*
 Exercise 3: Array.prototype.sort()
@@ -229,7 +232,7 @@ Hint: Use the String.prototype.split() method to separate the first and last
 // Complete the exercise in the space below:
 const firstLast = people.map((person) => {
     const [last, first] = person.split (',') 
-    return `${last} ${first}`
+    return `${first} ${last}`
 
 });
 // Check your work:
